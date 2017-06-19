e2477514a6399372d80b65938ccaf1e39ab1cb20
febbyfeysyah <febbyfeysyah@gmail.com>
2017-06-19 09:59:07 +0700

    merge databaseseeder conflict

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(PendudukSeed::class);
-        $this->call(UserSeeder::class);
+        // $this->call(UserSeeder::clasxss);
     }
 }
